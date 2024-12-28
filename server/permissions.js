2843e150fc37486b8529869d32171d0ecc84162e
NagyDoesStuff <91905724+NagameRlol@users.noreply.github.com>
2024-12-28 16:37:28 +0100

    Add files via upload

---
+++ server/permissions.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        "key": process.env.TOKEN_4,
+        "discordID": "0",
+        "nameColor": "#ff4a4a",
+        "class": "developer",
+        "infiniteLevelUp": true,
+        "name": "nagydoesstuff",
+		"administrator": true,
+        "note": "note here"
+    },
+]
